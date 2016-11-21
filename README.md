f1e805376266f373de669e751c9271f386080ae3
Lee Ravenberg <me@lravenberg.com>
2016-11-21 20:25:54 +0100

    configured doctrine migrations and fixtures. Upd8ed readme.md

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 fleetcombat-backend
 ===================
 
-A Symfony project created on November 20, 2016, 2:51 pm.
+The back-end is an REST API built with the Symfony framework.
+
+#How to setup:
+Make sure to have Composer installed globally on your computer. Then clone this repository.
+
+After that run the `composer install` command to install all dependencies.
+
+Make the database connection by editing `/app/config/parameters.yml`. You can copy the `parameters.yml.dist` file if 
+there is no file created yet. On your local machine it may look something like this:
+```yml
+parameters:
+    database_host: 127.0.0.1
+    database_port: null
+    database_name: fleetcombat-backend
+    database_user: root
+    database_password: root
+```
+
+#Database tables:
+Migrations are used to create the database schema. Then run the migrations with the bin/console doctrine:migrations:migrate command. For more about migrations read the docs: http://symfony.com/doc/current/bundles/DoctrineMigrationsBundle/index.html
---
+++ src/AppBundle/Entity/Player.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Player
+ *
+ * @ORM\Table(name="player")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PlayerRepository")
+ */
+class Player
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Player
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Player
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Player
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
+
